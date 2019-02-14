3f0d35bdfc52e03574ca8df25812b8f10b681ecf
ditohafizh <ditohafizh@gmail.com>
2019-02-14 15:29:18 +0700

    make pow function

--- calculator.js
+++ calculator.js
@@ -1,5 +1,17 @@
 function multiply(a, b){
     console.log(a*b);
 
-return (a * b);
+    return (a * b);
+}
+
+function pow(a, b) {
+    var sum = 0;
+    for (let i = 0; i < b; ++i) {
+        if (i = 0) {
+            sum = a;
+        } else {
+            sum *= a;
+        }
+    }
+    return sum;
 }

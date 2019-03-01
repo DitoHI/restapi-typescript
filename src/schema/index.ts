5d530dd5ed4662d48dd636956074232c1a5e8d3e
ditohafizh <ditohafizh@gmail.com>
2019-03-01 20:36:38 +0700

    update gitignore

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,5 +1,4 @@
-import { foodSchema } from './food';
 import { historyModel, IHistory } from './history';
 import { IUser, userModel } from './user';
 
-export { historyModel, IHistory, IUser, foodSchema, userModel };
+export { historyModel, IHistory, IUser, userModel };

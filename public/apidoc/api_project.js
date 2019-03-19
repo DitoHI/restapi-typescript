14ff78d7cabbc961f8a31899cee1130e232145de
ditohafizh <ditohafizh@gmail.com>
2019-03-20 05:01:43 +0700

    update gitignore

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "ToDo App",
+  "version": "1.0.0",
+  "description": "API of ToDo App",
+  "template": {
+    "forceLanguage": "en"
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-03-19T21:59:30.744Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
